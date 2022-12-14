7b46aea63dd36e56e86de61de81dc2d6b86a7f7f
Jack Rappazzo <jrappazzo@litmus.com>
2022-12-14 12:01:54 -0500

    Day 6 pt1

---
+++ Advent6/ElfPacketReader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent6
+{
+    class ElfPacketReader
+    {
+        string _packet;
+        public ElfPacketReader(string packet)
+        {
+            _packet = packet;
+        }
+
+        public int GetStart()
+        {
+            bool startFound = false;
+            int count = 3;
+            while (!startFound && count < _packet.Length)
+            {
+                startFound = IsMarker(_packet, count);
+                count++;
+            }
+            if (!startFound)
+                return -1;
+            else return count;
+        }
+
+
+        bool IsMarker(string input, int position)
+        {
+            if (position < 3)
+                throw new ArgumentOutOfRangeException("Position must be greater than three");
+            string segment = input.Substring(position - 3, 4);
+
+            for (int i = 0; i < segment.Length; i++)
+            {
+                char toCheck = segment[i];
+                for(int j=i+1; j<segment.Length; j++)
+                {
+                    if (segment[j] == toCheck)
+                        return false;
+
+                }
+            }
+            return true;
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Advent6/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Advent6;
+using Advent6.Resources;
+
+Console.WriteLine("Hello, radio operator!");
+Console.WriteLine("Tuning...");
+ElfPacketReader reader = new ElfPacketReader(Resource.Input);
+Console.WriteLine("Start of stream: " + reader.GetStart());
+Console.WriteLine("Press any key to finish");
+Console.ReadKey();
